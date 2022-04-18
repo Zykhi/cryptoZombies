17f75616a0e351e3816c08e245ead9ab4538f67d
Charly <zykhioff@gmail.com>
2022-04-18 12:01:31 +0200

    add mapping

--- zombieownership.sol
+++ zombieownership.sol
@@ -5,30 +5,33 @@ import "./erc721.sol";
 
 contract ZombieOwnership is ZombieAttack, ERC721 {
 
-  function balanceOf(address _owner) public view returns (uint256 _balance) {
-    return ownerZombieCount[_owner];
-  }
+    mapping (uint => address) zombieApprovals;
 
-  function ownerOf(uint256 _tokenId) public view returns (address _owner) {
-    return zombieToOwner[_tokenId];
-  }
+    function balanceOf(address _owner) public view returns (uint256 _balance) {
+        return ownerZombieCount[_owner];
+    }
 
-  function _transfer(address _from, address _to, uint256 _tokenId) private {
-    ownerZombieCount[_to]++;
-    ownerZombieCount[_from]--;
-    zombieToOwner[_tokenId] = _to;
-    Transfer(_from, _to, _tokenId);
-  }
+    function ownerOf(uint256 _tokenId) public view returns (address _owner) {
+        return zombieToOwner[_tokenId];
+    }
 
-  function transfer(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) {
-    _transfer(msg.sender, _to, _tokenId);
-  }
+    function _transfer(address _from, address _to, uint256 _tokenId) private {
+        ownerZombieCount[_to]++;
+        ownerZombieCount[_from]--;
+        zombieToOwner[_tokenId] = _to;
+        Transfer(_from, _to, _tokenId);
+    }
 
-  function approve(address _to, uint256 _tokenId) public {
+    function transfer(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) {
+        _transfer(msg.sender, _to, _tokenId);
+    }
 
-  }
+    function approve(address _to, uint256 _tokenId) public onlyOwnerOf(_tokenId) {
+        zombieApprovals[_tokenId] = _to;
+        Approval(msg.sender, _to, _tokenId);
+    }
 
-  function takeOwnership(uint256 _tokenId) public {
+    function takeOwnership(uint256 _tokenId) public {
 
-  }
+    }
 }
